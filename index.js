56c54b276885b94b7fca620fde5f5f0d8e1c3e06
Danil Shulga <ya.asd-danil@yandex.ru>
2024-03-01 21:50:12 +0300

    fix path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { throttle } from './utils/throttle.js';
-import { checkPosition } from './utils/checkPosition.js';
-import { fetchPosts } from './Post/fetchPosts.js';
-import { searchSubmit } from './Search/searchSubmit.js';
+import { throttle } from './src/scripts/utils/throttle.js';
+import { checkPosition } from './src/scripts/utils/checkPosition.js';
+import { fetchPosts } from './src/scripts/Post/fetchPosts.js';
+import { searchSubmit } from './src/scripts/Search/searchSubmit.js';
 
 const checkPositionThrottle = throttle(checkPosition, 200);
 window.addEventListener('scroll', () => checkPositionThrottle(fetchPosts));
